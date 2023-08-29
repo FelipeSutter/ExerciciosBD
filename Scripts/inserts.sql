caf274b7baaeb664ea2988f4e227dcc02cbfc16d
Felipe Sutter <lipeps61@gmail.com>
2023-08-29 15:57:47 -0300

    Insert da tabela livro

--- Scripts/inserts.sql
+++ Scripts/inserts.sql
@@ -22,4 +22,22 @@ values
 	('Suspense'),
 	('Infanto-Juvenil'),
 	('Técnico');
+
+-- Insert da tabela Livro
+
+insert into livro(id_livro,titulo_livro,preco_livro,estoque,editora,id_genero)
+values
+	(135, 'O Alquimista', '13.90' ,25, 'Planeta', 2),
+	(234, 'Onze Minutos', '11.90' ,0, 'Pergaminho', 3),
+	(345, 'Manuscrito Encontrado em Accra', '24.99' ,31, 'Sextante', 2),
+	(421, 'A Hora da Estrela', '13.80' ,12, 'Rocco', 1),
+	(567, 'Água Viva', '15.90' ,0, 'Rocco', 1),
+	(236, 'O mistério do coelho pensante e outros contos', '26.40' ,20, 'Rocco', 5),
+	(622, 'Gabriela, Cravo e Canela', '17.00' ,15, 'Companhia das Letras', 1),
+	(715, 'Dom Casmurro', '15.00' ,45, 'Globo Editora', 1),
+	(368, 'Sistema de Banco de Dados, 6e', '219.90' ,48, 'Elsevier', 6),
+	(123, 'Sistemas Operacionais com Java', '101.99' ,27, 'Elsevier', 6),
+	(908, 'Sistema de Banco de Dados', '86.50' ,15, 'Elsevier', 6),
+	(775, 'Fundamentos de Sistemas Operacionais', '130.15' ,25, 'LTC', 6);
+	
 	
