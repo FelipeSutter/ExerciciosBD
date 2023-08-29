7c4a43f256d44c5e7d3c9b944a73728f109a6d14
Felipe Sutter <lipeps61@gmail.com>
2023-08-29 15:34:20 -0300

    Insert da Tabela Autor

---
+++ inserts.sql
@@ -0,0 +1,14 @@
+-- Insert da tabela autor
+
+insert into autor (nome_autor,data_nasc_autor)
+values
+	('Jorge Amado', '19820810'),
+	('Machado de Assis', '18390621'),
+	('Paulo Coelho', '19470824'),
+	('Clarice Lispector', '19201210'),
+	('Abraham Silberschatz', '19540203'),
+	('Henry F. Korth S.', '19650130'),
+	('S. Sudarshan', '19671011'),
+	('Peter Baer Galvin', '19600923'),
+	('Greg Gagne', '19750415');
+	
