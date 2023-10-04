7363c851432d3f2d2c963e8a61debac99deae542
Felipe Sutter <lipeps61@gmail.com>
2023-10-04 16:45:49 -0300

    Consultas adicionais

--- Scripts/select.sql
+++ Scripts/select.sql
@@ -122,6 +122,14 @@ having max(i.quantidade) = 3;
 -- versão alternativa
 
 select 
+	 l.titulo_livro, l.id_livro, max(i.quantidade)  
+from livro l
+inner join itemvenda i on
+	l.id_livro = i.id_livro 
+where i.quantidade = (select max(quantidade) from itemvenda)
+group by l.id_livro ;
+   
+   select 
      l.titulo_livro, l.id_livro, max(i.quantidade)
 from livro l
 inner join itemvenda i on
@@ -130,10 +138,64 @@ inner join itemvenda i on
     having max(i.quantidade)
     order by i.quantidade desc
     limit 1;
+   
+-- 10. Quem comprou o livro mais caro?
+   
+select 
+   	c.nome_cliente, round(max(l.preco_livro))
+from cliente c 
+inner join venda v on
+	c.id_cliente = v.id_cliente 
+inner join itemvenda i on
+	v.id_venda = i.id_venda 
+inner join livro l on
+	i.id_livro = l.id_livro 
+where l.preco_livro = (select max(preco_livro) from livro);
+group by c.id_cliente;
 
+-- 11. Qual autor tem o livro mais barato?
 
+select 
+	a.nome_autor, min(l.preco_livro)
+from autor a 
+inner join escreve e on
+	a.id_autor = e.id_autor 
+inner join livro l on
+	e.id_livro = l.id_livro 
+where l.preco_livro = (select min(preco_livro) from livro);
 
+-- 12. Quantos titulos da editora "Rocco" estão cadastrados na loja?
 
+select 
+	count(l.id_livro), l.titulo_livro, l.editora  
+from livro l 
+where l.editora like 'Rocco'
+group by l.id_livro;
+
+-- 13. Qual a média de preço dos livros "Técnico" (genero)?
+
+select 
+	round(avg(l.preco_livro), 2), l.titulo_livro, g.descricao  
+from genero g 
+inner join livro l on
+	g.id_genero = l.id_genero 
+where g.descricao like 'Técnico';
+
+-- 14. Qual seria o valor total para se comprar um exemplar de cada livro da editora "Rocco"?
+
+select 
+	round(SUM(l.preco_livro),2), l.titulo_livro 
+from genero g 
+inner join livro l on
+	g.id_genero = l.id_genero 
+where l.editora like 'Rocco';
+
+-- 15. Qual o cliente, titulo do livro e autor da venda mais antiga?
+
+select 
+	
+from cliente c 
+inner join 
 
 
 
